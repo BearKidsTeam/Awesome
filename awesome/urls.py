b3607e0c3e4ff0eb65f313a4c3843600d08f0d9f
BLumia <wzc782970009@qq.com>
2016-04-02 23:34:55 +0800

    Router

---
+++ awesome/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.thread_list, name='thread_list'),
+]
--- awesome/views.py
+++ awesome/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def thread_list(request):
+	return render(request, 'awesome/thread_list.html', {})
