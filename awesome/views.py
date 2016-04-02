a7f3ea2fcb38f8486e319cc56b24ce21b299f9a4
BLumia <wzc782970009@qq.com>
2016-04-03 01:21:45 +0800

    Thread List Page

--- awesome/views.py
+++ awesome/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def thread_list(request):
-	return render(request, 'awesome/thread_list.html', {})
+	threads = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'awesome/thread_list.html', {'threads': threads})
