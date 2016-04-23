644169069956f13669148d49d495afaee0c79fdf
BLumia <wzc782970009@qq.com>
2016-04-24 00:01:09 +0800

    Tag List

--- awesome/urls.py
+++ awesome/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.thread_list, name='home_page'),
-	url(r'^thread/list/$', views.thread_list, name='thread_list'),
-	url(r'^app/list/$', views.app_list, name='app_list'),
+	url(r'^thread/$', views.thread_list, name='thread_list'),
+	url(r'^app/$', views.app_list, name='app_list'),
 	url(r'^thread/(?P<pk>[0-9]+)/$', views.thread_detail, name='thread_detail'),
 	url(r'^app/(?P<pk>[0-9]+)/$', views.app_detail, name='app_detail'),
+	url(r'^tag/$', views.tag_list, name='tag_list'),
 	url(r'^tag/(?P<pk>[0-9]+)/$', views.tag_id, name='tag_id'),
 ]
