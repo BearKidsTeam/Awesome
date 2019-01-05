3862fed0545e11150347a960d3c99a0425f0f11f
Gary Wang <wzc782970009@gmail.com>
2019-01-05 14:08:27 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    if env.get('CI'):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.test")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
 
